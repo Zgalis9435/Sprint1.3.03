c665e2e41db18cddd7f38a16cdb41b6f5f13c335
Zgalis9435 <zharos.delgado@protonmail.com>
2023-01-21 16:11:41 +0100

    Creation

---
+++ countriesProject/src/countriesProject/CountriesMain.java
@@ -0,0 +1,10 @@
+package countriesProject;
+
+public class CountriesMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ countriesProject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ZGali
+ *
+ */
+module countriesProject {
+}
